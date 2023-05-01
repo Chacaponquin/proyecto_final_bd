877baae6ac24d253cf499cbea713aee1a2e06393
Hector Angel Gomez Robaina <hectorangel2001@gmail.com>
2023-04-30 23:20:50 -0400

    Add Services

---
+++ src/cu/edu/cujae/structbd/services/CoachServices.java
@@ -0,0 +1,4 @@
+package cu.edu.cujae.structbd.services;
+
+public class CoachServices {
+}
--- src/cu/edu/cujae/structbd/services/ServicesLocator.java
+++ src/cu/edu/cujae/structbd/services/ServicesLocator.java
@@ -1,4 +1,17 @@
 package cu.edu.cujae.structbd.services;
 
-public class ServicesLocator {
+
+
+public abstract class ServicesLocator {
+    public static BatterServices BatterServices = new BatterServices();
+    public static CoachServices CoachServices = new CoachServices();
+    public static GameServices GameServices = new GameServices();
+    public static PhaseServices PhaseServices = new PhaseServices();
+    public static PitcherServices PitcherServices = new PitcherServices();
+    public static PlayerServices PlayerServices = new PlayerServices();
+    public static PositionServices PositionServices = new PositionServices();
+    public static ProvinceServices ProvinceServices = new ProvinceServices();
+    public static StadiumServices StadiumServices = new StadiumServices();
+    public static TeamServices TeamServices = new TeamServices();
+
 }
